95a22c27c86e1255d151d81a1e4116787ad48afc
irene <irene.monzonm@gmail.com>
2025-04-03 14:21:04 +1100

    configurate prisma and neon serveless, create product details page and product images

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,12 @@
 "use server"
-import { PrismaClient } from "@prisma/client"
+import { prisma } from '@/db/prisma' 
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../contants";
+
 
 //Get latest products
 
 export  async function getLatestProducts(){
-    const prisma=new PrismaClient();
 
     const data = await prisma.product.findMany({
         take:LATEST_PRODUCTS_LIMIT,
@@ -16,3 +16,9 @@ export  async function getLatestProducts(){
     return convertToPlainObject(data)
 }
 
+// Get single product by it's slug
+export async function getProductBySlug(slug:string){
+    return await prisma.product.findFirst({
+        where:{slug:slug}
+    });
+}
