591b9ffc2554309db77e915a1dcd9dc5c42edd8e
irene <irene.monzonm@gmail.com>
2025-04-26 16:44:38 +1000

    action to add card and toast

---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { Plus } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {CartItem} from '@/types'
+import { toast } from "sonner"
+import { addItemToCart } from '@/lib/actions/cart.actions'
+
+
+
+const  AddToCart= ({item}:{item:CartItem}) => {
+
+  const router=useRouter();
+  
+
+  const handleAddToCard=async()=>{
+    const res= await addItemToCart(item);
+
+    if(!res.success){
+      toast.error(res.message)
+      return
+    }
+    toast.success(`${item.name} added to cart`,
+      {
+        action: {
+  
+          label: "Go to Cart",
+          onClick:() => router.push('/cart'),
+        },
+      }
+    )
+  
+  }
+
+  return (
+    <Button
+      className='w-full'
+      type='button'
+      onClick={handleAddToCard}
+    >
+      <Plus/> Add to cart
+    </Button>
+
+  )
+}
+
+export default AddToCart
