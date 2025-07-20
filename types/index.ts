f1cab3988befeae4cc714290c8847e6c9c0631f8
irene <irene.monzonm@gmail.com>
2025-07-20 17:26:29 +1000

    created type validator  for orderSchema and order item schema

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,13 @@
 import{z} from 'zod'
-import { insertProductSchema,insertCartSchema,cartItemSchema,shippingAddressSchema } from '@/lib/validators';
+import { 
+    insertProductSchema,
+    insertCartSchema,
+    cartItemSchema,
+    shippingAddressSchema,
+    insertOrderItemSchema,
+    insertOrderSchema
+} from '@/lib/validators';
+
 
 export type Product=z.infer<typeof insertProductSchema> & {
     id:string;
@@ -11,3 +19,16 @@ export type Cart=z.infer<typeof insertCartSchema>
 export type CartItem=z.infer<typeof cartItemSchema>
 
 export type ShippingAddress=z.infer<typeof shippingAddressSchema>
+
+export type OrderItem=z.infer<typeof insertOrderItemSchema>
+
+export type Order = z.infer<typeof insertOrderSchema>& {
+    id:string;
+    createAt:Date;
+    isPaid:boolean;
+    paidAt:Date | null;
+    isDelivered: boolean;
+    deliveredAt:Date|null;
+    orderitems:OrderItem[];
+    user:{name:string;email:string}
+}
