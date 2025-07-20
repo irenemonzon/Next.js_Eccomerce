e16393fbe83560c0f6d2802f829c9b0f238a4beb
irene <irene.monzonm@gmail.com>
2025-07-20 17:47:55 +1000

    fix deploy

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from "next"
 import { auth } from "@/auth"
+import { Metadata } from "next"
 import { getUserById } from "@/lib/actions/user.actions"
 import PaymentMethodForm from "./payment-method-form"
 import CheckoutSteps from "@/components/shared/checkout-steps"
 
-export const metada:Metadata={
+export const metadata:Metadata={
     title:'Select Payment Method'
 }
 
