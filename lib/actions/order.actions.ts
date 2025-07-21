bcc38008204dbb80c8e1696e10ca292ef5e68a29
irene <irene.monzonm@gmail.com>
2025-07-21 10:05:29 +1000

    create order action to triggers the order

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,78 @@
+'use server'
+
+import { isRedirectError } from "next/dist/client/components/redirect-error"
+import { formatError } from "../utils"
+import { auth } from "@/auth"
+import { getMyCar } from "./cart.actions"
+import { getUserById } from "./user.actions"
+import { insertOrderSchema } from "../validators"
+import { prisma } from "@/db/prisma"
+import { CartItem } from "@/types"
+
+//Order and order item
+
+export async function createOrder(){
+    try{
+
+        const session=await auth()
+        if(!session) throw new Error('User is not authenticated')
+
+        const cart= await getMyCar();
+        const userId= session?.user?.id
+        if(!userId) throw new Error('User not found')
+
+        const user=await getUserById(userId);
+        if(!cart|| cart.items.length===0){
+            return {success:false,message:'Your cart is empty',redirecTo:'/cart' }
+        }
+         if(!user.address){
+            return {success:false,message:'No shipping address',redirecTo:'/shipping-address' }
+        }
+         if(!user.paymentMethod){
+            return {success:false,message:'No payment method',redirecTo:'/payment-method' }
+        }
+
+        const order=insertOrderSchema.parse({
+            userId:user.id,
+            shippingAddress:user.address,
+            paymentMethod:user.paymentMethod,
+            itemsPrice:cart.itemsPrice,
+            shippingPrice:cart.shippingPrice,
+            taxPrice:cart.taxPrice,
+            totalPrice:cart.totalPrice,
+        })
+
+        //Create a transaction
+        const insertedOrderId=await prisma.$transaction(async(tx)=>{
+           const insertedOrder= await tx.order.create({data:order})
+
+           for(const item of cart.items as CartItem[]){
+            await tx.orderItem.create({
+                data:{
+                    ...item,
+                    price:item.price,
+                    orderId:insertedOrder.id
+                }
+            })
+           }
+           //Clear cart
+           await tx.cart.update({
+            where:{id:cart.id},
+            data:{
+                items:[],
+                totalPrice:0,
+                taxPrice:0,
+                shippingPrice:0,
+                itemsPrice:0
+             }
+           })
+           return insertedOrder.id;
+        })
+        if(!insertedOrderId)throw new Error('Order not created')
+        return {success:true,message:'Order created', redirectTo:`/order/${insertedOrderId}`}
+    }catch(error){
+        if(isRedirectError(error))throw error
+        return {success:false, message:formatError(error)}
+
+    }
+}
--- lib/validators.ts
+++ lib/validators.ts
@@ -88,7 +88,7 @@ export const paymentMethodSchema= z.object({
 // Schema for inserting order
 export const insertOrderSchema=z.object({
   userId:z.string().min(1,'User is required'),
-  itemPrice:currency,
+  itemsPrice:currency,
   shippingPrice:currency,
   taxPrice:currency,
   totalPrice:currency,
