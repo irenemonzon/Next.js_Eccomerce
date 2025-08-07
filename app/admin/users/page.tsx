15536f6d3d111ca791d45a64da54f6c4a7341df3
irene <irene.monzonm@gmail.com>
2025-08-07 15:07:30 +1000

    create action for all users and create table for admin users

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,82 @@
+import { requireAdmin } from "@/lib/auth-guard"
+import { Metadata } from "next"
+import { getAllUsers } from "@/lib/actions/user.actions"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import Deletedialog from "@/components/shared/delete-dialog"
+import Pagination from "@/components/shared/pagination"
+import { formatId } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+
+export const metadata:Metadata={
+    title:'Admin users'
+}
+
+
+
+const AdminUsersPage = async(props:{
+    searchParams:Promise<{
+        page:string
+    }>
+}) => {
+    await requireAdmin()
+
+    const {page='1'}= await props.searchParams
+
+    const users= await getAllUsers({page:Number(page)})
+    
+
+  return (
+     <div className='space-y-2'>
+            <h2 className='h2-bold'>Users</h2>
+            <div className='overflow-x-auto'>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>ID</TableHead>
+                            <TableHead>NAME</TableHead>
+                            <TableHead>EMAIL</TableHead>
+                            <TableHead>ROLE</TableHead>
+                            <TableHead>ACTIONS</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.data.map((user)=>(
+                            <TableRow key={user.id}>
+                                <TableCell>{formatId(user.id)}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>
+                                    {user.role ==='user'? (
+                                        <Badge variant='secondary'>user</Badge>
+
+                                    ):(
+                                        <Badge variant='default'>Admin</Badge>
+                                    )}
+                                </TableCell>
+                                <TableCell className="flex gap-4">
+                                    <Button asChild variant='outline' size='sm'>
+                                        <Link href={`/admin/users/${user.id}`}>
+                                            Edit
+                                        </Link>
+                                    </Button>   
+                                    {/* <Deletedialog id={user.id} action={deleteOrder}/> */}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                {users.totalPages>1 &&(
+                    <Pagination 
+                        page={Number(page)|| 1}
+                        totalPages={users?.totalPages}
+                    />
+
+                )}
+            </div>
+        </div>
+  )
+}
+
+export default AdminUsersPage
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,6 +9,7 @@ import {prisma} from '@/db/prisma'
 import { formatError } from "../utils"
 import { ShippingAddress } from "@/types"
 import { z } from "zod"
+import { PAGE_SIZE } from "../contants"
 
 
 
@@ -180,6 +181,26 @@ export async function updateProfile(user:{name:string,email:string}){
 
     }catch(error){
         return {success:false, message:formatError(error)}
+    }
+
+}
+
+export async function getAllUsers({
+    limit=PAGE_SIZE,
+    page
+}:{
+    limit?:number,
+    page:number
+}){
+    const data= await prisma.user.findMany({
+        orderBy:{createdAt: 'desc'},
+        take:limit,
+        skip:(page -1)* limit
+    })
+    const dataCount=await prisma.user.count()
+    return{
+        data,
+        totalPages:Math.ceil(dataCount/limit)
     }
 
 }
